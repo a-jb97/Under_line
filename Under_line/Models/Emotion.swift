3aff5a40398395a7921ef116b229d144df6bdd8c
a-jb97 <a_jb97@naver.com>
2026-03-25 17:42:57 +0900

    Feat : Emotion 타입 구현

---
+++ Under_line/Models/Emotion.swift
@@ -0,0 +1,34 @@
+//
+//  Emotion.swift
+//  Under_line
+//
+//  문장 수집 시 선택하는 감정 타입
+//
+
+import UIKit
+
+enum Emotion: Int, CaseIterable {
+    case joy, calm, sad, touched, pensive, tense
+
+    var emoji: UIImage {
+        switch self {
+        case .joy:     return .happy
+        case .calm:    return .calm
+        case .sad:     return .sad
+        case .touched: return .moved
+        case .pensive: return .meditation
+        case .tense:   return .nervous
+        }
+    }
+
+    var label: String {
+        switch self {
+        case .joy:     return "기쁨"
+        case .calm:    return "평온"
+        case .sad:     return "슬픔"
+        case .touched: return "감동"
+        case .pensive: return "사색"
+        case .tense:   return "긴장"
+        }
+    }
+}
