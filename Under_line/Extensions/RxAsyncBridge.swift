8cb1eb3df55b530bc27ed719f3f2b12c19590e79
a-jb97 <a_jb97@naver.com>
2026-04-20 17:53:28 +0900

    Refact: rxAsync 브릿지 헬퍼 추가 및 AladinAPIService async 전환

---
+++ Under_line/Extensions/RxAsyncBridge.swift
@@ -0,0 +1,25 @@
+//
+//  RxAsyncBridge.swift
+//  Under_line
+//
+//  async throws 함수를 RxSwift Observable로 감싸는 헬퍼
+//
+
+import RxSwift
+
+/// async throws 작업을 Observable<T>로 래핑한다.
+/// Task가 취소되면 Disposable 해제 시점에 cancel된다.
+func rxAsync<T>(_ work: @escaping () async throws -> T) -> Observable<T> {
+    Observable.create { observer in
+        let task = Task {
+            do {
+                let result = try await work()
+                observer.onNext(result)
+                observer.onCompleted()
+            } catch {
+                observer.onError(error)
+            }
+        }
+        return Disposables.create { task.cancel() }
+    }
+}
